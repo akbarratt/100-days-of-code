2bb8965233ed96d8128abd80243c5151cc6d684e
Andria Barratt <andria@akbarratt.com>
2020-05-06 23:03:06 -0400

    Update app.js
    Rewrite in line with Day 8 tutorial

--- exercises/DOM-Manipulation/app.js
+++ exercises/DOM-Manipulation/app.js
@@ -1,51 +1,29 @@
-// document.getElementsByClassName
+// Create element
+const li = document.createElement('li');
 
-// const items = document.getElementsByClassName('collection-item');
-// console.log(items);
-// console.log(items[0]);
-// items[0].style.color= 'red';
-// items[3].textContent = 'Hello';
+// Add class
+li.className = 'collection-item';
 
-// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// Add ID
+li.id = 'new-item';
 
-// console.log(listItems);
+// Add attribute
+li.setAttribute('title', 'New Item');
 
-// document.getElementsByTagName
-// let lis = document.getElementsByTagName('li');
-// console.log(lis);
-// console.log(lis[0]);
-// lis[0].style.color= 'red';
-// lis[3].textContent = 'Hello';
+// Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
 
-// // Convert HTML Collection into array
+// Create new link element
+const link = document.createElement('a');
+//Add classes
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
 
-// lis = Array.from(lis);
+// Append link into li
+li.appendChild(link);
 
-// lis.reverse();
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
 
-// lis.forEach(function(li, index){
-//   console.log(li.className);
-//   li.textContent = `${index}: Hello`;
-// });
-
-// console.log(lis);
-
-// document.querySelectorAll
-const items = document.querySelectorAll('ul.collection li.collection-item');
-
-items.forEach(function(item, index){
-  item.textContent = `${index}: Hello`
-})
-
-const liOdd = document.querySelectorAll('li:nth-child(odd)');
-const liEven = document.querySelectorAll('li:nth-child(even)');
-
-liOdd.forEach(function(li, index){
-  li.style.background = '#ccc';
-})
-
-for(let i = 0; i < liEven.length; i++) {
-  liEven[i].style.background = '#f4f4f4';
-}
-
-console.log(items);
+console.log(li);
