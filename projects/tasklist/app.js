9a933c689e17a5e69f4405cc52d1d10b40e6a133
akbarratt <andria@akbarratt.com>
2020-05-24 00:10:42 -0400

    Fix bugs and change style.

--- projects/tasklist/app.js
+++ projects/tasklist/app.js
@@ -2,18 +2,9 @@
 
 let listBox = document.querySelector('#list-box');
 
-let lis = document.querySelectorAll ('li');
-
 let xButton = document.getElementsByClassName('x');
 
 let xButtons = Array.from(xButton);
-
-let xLink = document.createElement('a');
-xLink.textContent = 'X';
-xLink.className = 'x';
-xLink.setAttribute('href', '');
-
-console.log(xLink);
 
 let list = document.querySelector('ul');
 
@@ -21,27 +12,40 @@ let resetButton = document.querySelector('#clear-tasks');
 
 let addButton = document.querySelector('#add-task');
 
+let taskInput = document.querySelector("#task-input");
+
 // Store input in a variable
 
 let taskInputText = document.querySelector("#task-input").value;
-// let taskInputText = document.getElementById("task-input").value;
-
-// console.log(taskInputText);
 
 // Add event listener to add button
 
 addButton.addEventListener('click', addTask);
 
-function addTask (newTask){
+function addTask (e){
+  if(taskInput.value === '') {
+    alert('Add a task');
+  } else {
   // console.log(taskInputText);
-  newTask = document.createElement('li');
+  const newTask = document.createElement('li');
   // Add input text to newly created <li>
-  newTask.textContent = taskInputText;
-  // Append x button to newly created <li>
+  // newTask.textContent = taskInputText;
+  newTask.appendChild(document.createTextNode(taskInput.value));
+  // Create x button link
+  let xLink = document.createElement('a');
+  xLink.textContent = 'X';
+  xLink.className = 'x';
+  xLink.setAttribute('href', '');
+  // Add event listener to make x button delete <li> on click
+  xLink.addEventListener('click', deleteTask);
+  // Append x link to newly created <li>
   newTask.appendChild(xLink);
   // Add newly created li to ul
   list.appendChild(newTask);
   console.log(newTask);
+  // Clear input field
+  taskInput.value = '';
+  }
   if (list.childElementCount > 0) {
     list.parentElement.style.visibility = 'visible';
   }
@@ -68,3 +72,6 @@ function deleteTask(e) {
   }
 }
 
+// Garbage (I think)
+
+// let lis = document.querySelectorAll ('li');
