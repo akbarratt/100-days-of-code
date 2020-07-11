436c20bb684e4b723abeda8093c717a6ed1aaa68
akbarratt <andria@akbarratt.com>
2020-07-10 20:06:34 -0400

    Build out app interface and begin styling.

--- projects/loancalc/app.js
+++ projects/loancalc/app.js
@@ -1,3 +1,7 @@
-const test = document.querySelectorAll('#amt-input');
+// Variable declarations
 
-console.log(test);
+const amtInput = document.querySelectorAll('#amt-input').textContent;
+const intInput = document.querySelectorAll('#int-input').textContent;
+const yrsInput = document.querySelectorAll('#years-input').textContent;
+
+console.log(amtInput)
