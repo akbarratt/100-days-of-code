e4848e781f40291212f7aa70697bcf8e462362f5
akbarratt <andria@akbarratt.com>
2020-07-12 00:11:05 -0400

    Complete form, begin JS programming.

--- projects/loancalc/app.js
+++ projects/loancalc/app.js
@@ -1,7 +1,21 @@
 // Variable declarations
 
-const amtInput = document.querySelectorAll('#amt-input').textContent;
+// None of this is working. HTML collection vs node list?
+
+const amtInput = parseFloat(document.querySelectorAll('#amt-input').value);
 const intInput = document.querySelectorAll('#int-input').textContent;
 const yrsInput = document.querySelectorAll('#years-input').textContent;
 
-console.log(amtInput)
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e) {
+  console.log("submit");
+  calcMonthlyPayment();
+  e.preventDefault(); 
+  console.log(document.querySelectorAll('#amt-input').value)
+}
+
+function calcMonthlyPayment() {
+  let monthlyPayment = amtInput / 12;
+  console.log(monthlyPayment);
+}
