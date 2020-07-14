e2a03dcbfe285991f06b36c5641168a7ee2565e9
akbarratt <andria@akbarratt.com>
2020-07-14 16:38:17 -0400

    Complete calculator functionality. Correct errors, add clarity in UI.

--- projects/loancalc/app.js
+++ projects/loancalc/app.js
@@ -1,7 +1,7 @@
 // Variable declarations
 
 // Any work with the values have to be done inside the function because it requires user input.
-// const amtInput = document.querySelector('#amt-input');
+const amtInput = document.querySelector('#amt-input');
 const intInput = document.querySelector('#int-input');
 const yrsInput = document.querySelector('#years-input');
 const monthlyPayment = document.querySelector('#monthly-payment');
@@ -14,17 +14,32 @@ document.getElementById('loan-form').addEventListener('submit', calculateResults
 // Calculate results on submit
 function calculateResults(e) {
   e.preventDefault();
-  // if amtInput
-  // calcMonthlyPayment();
-  // calcTotalPayment();
-  // calcTotalInterest();
-  let amtInput = document.querySelector('#amt-input');
-  let principal = parseFloat(amtInput.value);
+  // How do I return an error? Because running any data type through parseFloat will force it to be a number.
+  const principal = parseFloat(amtInput.value);
+  const calculatedInterest = parseFloat(intInput.value) / 100 / 12;
+  const calculatedPayments = parseFloat(yrsInput.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal*x*calculatedInterest)/(x-1);
+  
   console.log(principal);
-  console.log(principal.typeof);
+  console.log(calculatedInterest);
+  console.log(calculatedPayments);
+  console.log(monthly);
+
+  if(isFinite(monthly)) {
+    monthlyPayment.innerHTML = monthly.toFixed(2);
+    totalPayment.innerHTML = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.innerHTML = ((monthly * calculatedPayments)-principal).toFixed(2);
+  } else {
+    alert('Error, please check your numbers.');
+  }
 }
 
-function calcMonthlyPayment() {
-  // let monthlyPayment = amtInput.value / 12;
-  // console.log(monthlyPayment);
-}
+// In this case it seems like isFinite serves the function I was trying to do, which was check whether the result of the form input was a number.
+
+// I was using typeof incorrectly. :)
+
+// To Do:
+// Input field of type "number" does not allow a decimal and trying to place one results in a browser error.
