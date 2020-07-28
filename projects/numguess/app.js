95a8bfd2a3cbbc84341e644ab2bd0d2e70e7ac36
akbarratt <andria@akbarratt.com>
2020-07-28 00:19:54 -0400

    Add comparison of user input to secret number.

--- projects/numguess/app.js
+++ projects/numguess/app.js
@@ -1,23 +1,24 @@
+// ____________
 // Declarations
 
 // Insert answer for testing purposes
 const testnum = document.getElementById('testnum');
 // User input (need to ensure this only accepts a value 1-10)
-const guessInput = document.getElementById('guess-input').value;
+const guessInput = document.getElementById('guess-input');
 // Submit button
 const submitBtn = document.getElementById('guess-value'); // this doesn't seem semantic to me
 
-//Getting started
+//_______________
+//Getting started (on page load/refresh)
 
-
+// Initialize generated number
+let generatedNumber;
 // Guesses baseline
 let guesses = 0;
-// Generated number
-function numGen() {
-  let generatedNumber = Math.ceil((Math.random() * 10))
-  testnum.textContent = generatedNumber;
-}
+// Run number generation function
+numGen();
 
+//_________________________
 // Load all event listeners
 // INSERT HERE
 
@@ -25,18 +26,33 @@ function numGen() {
 
 submitBtn.addEventListener('click', submitGuess);
 
+//__________
+// Functions
+
+// Number generation function
+function numGen() {
+  generatedNumber = Math.ceil((Math.random() * 10));
+  testnum.textContent = generatedNumber;
+  console.log(typeof(generatedNumber));
+}
+
+// Submit guess event handler
 function submitGuess(e) {
   let guess = parseInt(guessInput.value);
-  console.log(guessInput);
-  console.log(guess);
+  // parseInt should be sufficient for now because it rounds up
+  console.log('User input guess:' + guess);
   // If not a number 1-10, error
-  // Else, compare input to generatedNumber
-  // How to handle number of guesses? Increment? Where to store it?
-  guesses++;
+  if(guess > 10 || guess < 1){
+    console.log('Number must be 1-10.')
+  } else if (guess !== generatedNumber) {
+    console.log('incorrect guess', generatedNumber);
+    guesses++
+    console.log('Number of guesses:' + guesses);
+  } else {
+    console.log('correct');
+  }
+  e.preventDefault();
 }
 
 
 
-
-
-numGen();
