646075050d4a7b0ea44f9b7821b36066a9cb06be
akbarratt <andria@akbarratt.com>
2020-07-28 15:20:18 -0400

    Abstract game code.

--- projects/numguess/app.js
+++ projects/numguess/app.js
@@ -1,12 +1,12 @@
 // ____________
 // Declarations
 
-// Insert answer for testing purposes
-const testnum = document.getElementById('testnum');
 // User input (need to ensure this only accepts a value 1-10)
 const guessInput = document.getElementById('guess-input');
 // Submit button
 const submitBtn = document.getElementById('guess-value'); // this doesn't seem semantic to me
+// Win condition span
+const results = document.getElementById('results');
 
 //_______________
 //Getting started (on page load/refresh)
@@ -34,7 +34,6 @@ submitBtn.addEventListener('click', submitGuess);
 // Number generation function
 function numGen() {
   generatedNumber = Math.ceil((Math.random() * 10));
-  testnum.textContent = generatedNumber;
 }
 
 // Submit guess event handler
@@ -45,21 +44,34 @@ function submitGuess(e) {
   // If not a number 1-10, error
   if(guess > 10 || guess < 1){
     console.log('Number must be 1-10.')
-  } else if (guess !== generatedNumber) {
-    console.log('incorrect guess');
-    guesses++
-    console.log('Number of guesses:' + guesses);
   } else {
-    console.log('correct');
+    game();
   }
-  // Game Over condition (doesn't work outside of the game)
-  if (guesses >= 3) {
-    console.log('Game Over');
-  }
-  e.preventDefault();
   // Does the whole game have to run inside the event handler? Can this be abstracted? I'm clearly struggling to understand scope.
 }
 
+// Number guessing game
+function game(){
+  let guess = parseInt(guessInput.value);
+  if (guess !== generatedNumber) {
+    /*testing*/console.log('incorrect guess');
+    guesses++
+    results.textContent = 'Incorrect. ' + (3-guesses) + ' guess(es) remaining.';
+    /*testing*/console.log('Number of guesses:' + guesses);
+  } else {
+    /*testing*/console.log('correct');
+    results.textContent = 'Correct! You win!';
+  }
+  // Game Over condition (doesn't work outside of the game?? Would it work as var?)
+  if (guesses >= 3) {
+    /*testing*/console.log('Game Over');
+    results.textContent = 'Sorry, game over! 0 guesses remaining.';
+    generatedNumber = undefined;
+  }
+}
 
-
+// TO DO:
+// - Reset button
+// - I could pull in some functions to deal with the plurals / negative guesses but that could be complicated and overkill.
+// - How to tell what the correct answer was while ending the game and not allowing for further guesses? Would need to change the 'undefined' workaround in place now.
 
