70d2ebed382bc110c3daf0726d3e8587e708aa05
akbarratt <andria@akbarratt.com>
2020-07-30 23:19:24 -0400

    Add error messages. Fix bugs.

--- projects/numguess/app.js
+++ projects/numguess/app.js
@@ -7,6 +7,10 @@ const guessInput = document.getElementById('guess-input');
 const submitBtn = document.getElementById('guess-value'); // this doesn't seem semantic to me
 // Win condition span
 const results = document.getElementById('results');
+// Retry container
+const retryCont = document.getElementById('retry');
+// Retry button
+const retryBtn = document.getElementById('retry-btn');
 
 //_______________
 //Getting started (on page load/refresh)
@@ -27,6 +31,7 @@ console.log('Generated number: ' + generatedNumber);
 // Submit event handler
 
 submitBtn.addEventListener('click', submitGuess);
+retryBtn.addEventListener('click', refresh);
 
 //__________
 // Functions
@@ -36,18 +41,30 @@ function numGen() {
   generatedNumber = Math.ceil((Math.random() * 10));
 }
 
+// Refresh page button
+function refresh(){
+  window.location.reload();
+}
+
+// Display retry on game over
+function retry() {
+  retryCont.style.display = 'block';
+}
+
 // Submit guess event handler
 function submitGuess(e) {
   let guess = parseInt(guessInput.value);
   // parseInt should be sufficient for now because it rounds up, but eventually need to disallow decimals
   console.log('User input guess:' + guess);
   // If not a number 1-10, error
-  if(guess > 10 || guess < 1){
+  if(guess > 10 || guess < 1 || isNaN(guess)){
     console.log('Number must be 1-10.')
+    results.textContent = 'Number must be 1-10. '+ (3-guesses) + ' guess(es) remaining.';
+  } else if (guesses === null) {
+    results.textContent = 'You already won! '
   } else {
     game();
   }
-  // Does the whole game have to run inside the event handler? Can this be abstracted? I'm clearly struggling to understand scope.
 }
 
 // Number guessing game
@@ -58,15 +75,17 @@ function game(){
     guesses++
     results.textContent = 'Incorrect. ' + (3-guesses) + ' guess(es) remaining.';
     /*testing*/console.log('Number of guesses:' + guesses);
-  } else {
+  } else if (guess === generatedNumber) {
     /*testing*/console.log('correct');
     results.textContent = 'Correct! You win!';
-  }
-  // Game Over condition (doesn't work outside of the game?? Would it work as var?)
-  if (guesses >= 3) {
+    retry();
+    guesses = null;
+  } 
+  if (guesses >=3){
     /*testing*/console.log('Game Over');
-    results.textContent = 'Sorry, game over! 0 guesses remaining.';
+    results.textContent = 'Sorry, game over!';
     generatedNumber = undefined;
+    retry();
   }
 }
 
@@ -74,4 +93,5 @@ function game(){
 // - Reset button
 // - I could pull in some functions to deal with the plurals / negative guesses but that could be complicated and overkill.
 // - How to tell what the correct answer was while ending the game and not allowing for further guesses? Would need to change the 'undefined' workaround in place now.
-// - BUG: win condition not permanent. If winner guesses again, game continues.
+// - FEATURE: show which numbers have already been guessed and prevent guessing the same number twice (array?)
+// - NOTE: David thinks the HTML element can handle validation and require input.
