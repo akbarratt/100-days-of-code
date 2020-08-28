4f8529c6d846d64bdfac5afa911c0e2ba2344784
akbarratt <andria@akbarratt.com>
2020-08-27 21:26:26 -0400

    Demonstrate ready states.

--- ejs/ejs.js
+++ ejs/ejs.js
@@ -7,11 +7,26 @@ function loadData() {
   // OPEN
   xhr.open('GET', 'data.txt', true);
 
+  console.log('READYSTATE', xhr.readyState);
+
+  // Optional - used for spinners/loaders
+
+  xhr.onprogress = function(){
+    console.log('READYSTATE', xhr.readyState);
+  }
+
   xhr.onload = function(){
+    console.log('READYSTATE', xhr.readyState);
     if(this.status === 200) {
-      console.log(this.responseText);
+      // console.log(this.responseText);
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`
     }
   }
 
+  xhr.onerror = function () {
+    console.log('Request error...');
+  }
+
   xhr.send();
+  
 }
